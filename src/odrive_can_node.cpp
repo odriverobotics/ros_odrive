ae632620c99804084e984778fd3f7430c89d0f80
Jonas Land <jonasaustinland@tamu.edu>
2024-06-12 08:52:19 -0500

    updated value access response msg mutex name

--- src/odrive_can_node.cpp
+++ src/odrive_can_node.cpp
@@ -227,7 +227,7 @@ void ODriveCanNode::recv_callback(const can_frame& frame) {
 
         case CmdId::kTxSdo: {
             if (!verify_length("kTxSdo", 8, frame.can_dlc)) break;
-            std::lock_guard<std::mutex> guard(value_access_response_mutex_);
+            std::lock_guard<std::mutex> guard(value_access_response_msg_mutex_);
             value_access_response_msg_.float_value   = read_le<float>(frame.data + 4);
             value_access_response_publisher_->publish(value_access_response_msg_);
             break;
