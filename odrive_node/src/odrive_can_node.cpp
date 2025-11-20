e666ddf39632fb72e87ea6a244b010430a347135
Neppel Elian <elian.neppel@posteo.eu>
2025-11-20 20:45:40 +0900

    Macro changing behavior for RCLCPP > 18 (jazzy)

--- odrive_node/src/odrive_can_node.cpp
+++ odrive_node/src/odrive_can_node.cpp
@@ -44,10 +44,18 @@ ODriveCanNode::ODriveCanNode(const std::string& node_name) : rclcpp::Node(node_n
     subscriber_ = rclcpp::Node::create_subscription<ControlMessage>("control_message", ctrl_msg_qos, std::bind(&ODriveCanNode::subscriber_callback, this, _1));
 
     rclcpp::QoS srv_qos(rclcpp::KeepAll{});
-    service_ = rclcpp::Node::create_service<AxisState>("request_axis_state", std::bind(&ODriveCanNode::service_callback, this, _1, _2), srv_qos.get_rmw_qos_profile());
+    #if RCLCPP_VERSION_MAJOR >= 18
+        service_ = rclcpp::Node::create_service<AxisState>("request_axis_state", std::bind(&ODriveCanNode::service_callback, this, _1, _2), srv_qos);
+    #else
+        service_ = rclcpp::Node::create_service<AxisState>("request_axis_state", std::bind(&ODriveCanNode::service_callback, this, _1, _2), srv_qos.get_rmw_qos_profile());
+    #endif
 
     rclcpp::QoS srv_clear_errors_qos(rclcpp::KeepAll{});
-    service_clear_errors_ = rclcpp::Node::create_service<Empty>("clear_errors", std::bind(&ODriveCanNode::service_clear_errors_callback, this, _1, _2), srv_clear_errors_qos.get_rmw_qos_profile());
+    #if RCLCPP_VERSION_MAJOR >= 18
+        service_clear_errors_ = rclcpp::Node::create_service<Empty>("clear_errors", std::bind(&ODriveCanNode::service_clear_errors_callback, this, _1, _2), srv_clear_errors_qos);
+    #else
+        service_clear_errors_ = rclcpp::Node::create_service<Empty>("clear_errors", std::bind(&ODriveCanNode::service_clear_errors_callback, this, _1, _2), srv_clear_errors_qos.get_rmw_qos_profile());
+    #endif
 }
 
 void ODriveCanNode::deinit() {
